de015906c5725c85fca212697e3ea9c9d5b1163c
pedromsilva99 <pedromsilva99@ua.pt>
2021-12-07 17:28:59 +0000

    final version

--- multiplexer.vhd
+++ multiplexer.vhd
@@ -10,14 +10,7 @@ end Mux2_1;
 
 architecture BehavProc of Mux2_1 is
 begin
-    process(sel, dataln0, dataln1)
-    begin
-        if (sel = '0') then
-            out1 <= dataln0;
-        else
-            out1 <= dataln1;
-        end if;
-    end process;
+    out1 <= (dataln0 and not sel) or (dataln1 and sel) ;
 end BehavProc;
 
 
@@ -33,20 +26,6 @@ end Mux4_1;
 
 architecture BehavProc of Mux4_1 is
 begin
-    process(sel,selPar, dataln0, dataln1, dataln2, dataln3)
-    begin
-        if (sel = '0') then
-				if(selPar = '0')then
-					out1 <= dataln0;
-				elsif(selPar = '1')then
-					out1 <= dataln1;
-				end if;
-        elsif (sel = '1') then
-				if(selPar = '0')then
-					out1 <= dataln2;
-				elsif(selPar = '1')then
-					out1 <= dataln3;
-				end if;	
-        end if;
-    end process;
+    out1 <= (sel and selPar and dataln3) or (sel and not selPar and dataln2) 
+	 or (not sel and selPar and dataln1) or (not sel and not selPar and dataln0);
 end BehavProc;
