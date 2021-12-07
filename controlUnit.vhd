356ccb8169a6898fff2b233bbb570e58f06934b3
Dukk0 <tiagobarros99@ua.pt>
2021-12-07 17:10:40 +0000

    logic gates for control unit

--- controlUnit.vhd
+++ controlUnit.vhd
@@ -2,177 +2,20 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY controlUnit IS
-  PORT (state_counter: IN std_logic_vector(3 downto 0);
+  PORT (sc: IN std_logic_vector(3 downto 0);
         comp_rst, a, b, c, d, sel2_1, sel4_1, selPar:      OUT STD_LOGIC);
 END controlUnit;
 
 ARCHITECTURE behavior OF controlUnit IS
 	
 BEGIN
-	
-	PROCESS(state_counter)
-	BEGIN
-			IF	(state_counter = "0000") -- 0
-				then a <= '0';
-						b <= '0';
-						c <= '0';
-						d <= '0';
-						sel2_1 <= '0';
-						sel4_1 <= '0';
-						selPar <= '0';
-						comp_rst <='0';
-			elsif(state_counter = "0001") -- 1
-				then
-					a <= '1';
-					b <= '1';
-					c <= '0';
-					d <= '0';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "0010") --2
-				then
-					a <= '1';
-					b <= '0';
-					c <= '1';
-					d <= '0';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-
-			elsif(state_counter = "0011") --3
-				then
-					a <= '1';
-					b <= '0';
-					c <= '0';
-					d <= '1';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "0100") --4
-				then
-					a <= '0';
-					b <= '1';
-					c <= '1';
-					d <= '0';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "0101") --5
-				then
-					a <= '0';
-					b <= '1';
-					c <= '0';
-					d <= '1';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "0110") --6
-				then
-					a <= '0';
-					b <= '0';
-					c <= '1';
-					d <= '1';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "0111") --7
-				then
-					a <= '1';
-					b <= '1';
-					c <= '1';
-					d <= '0';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "1000") --8
-				then
-					a <= '1';
-					b <= '1';
-					c <= '0';
-					d <= '1';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "1001") --9
-			then
-					a <= '1';
-					b <= '0';
-					c <= '1';
-					d <= '1';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "1010") --10
-				then
-					a <= '0';
-					b <= '1';
-					c <= '1';
-					d <= '1';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "1011") --11
-				then
-					a <= '1';
-					b <= '1';
-					c <= '1';
-					d <= '1';
-					sel2_1 <= '0';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "1100") --12
-				then
-					a <= '0';
-					b <= '0';
-					c <= '0';
-					d <= '0';
-					sel2_1 <= '1';
-					sel4_1 <= '0';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "1101") --13
-				then
-					a <= '0';
-					b <= '0';
-					c <= '0';
-					d <= '0';
-					sel2_1 <= '1';
-					sel4_1 <= '0';
-					selPar <= '1';
-					comp_rst <='1';
-			elsif(state_counter = "1110") --14
-				then
-					a <= '0';
-					b <= '0';
-					c <= '0';
-					d <= '0';
-					sel2_1 <= '1';
-					sel4_1 <= '1';
-					selPar <= '0';
-					comp_rst <='1';
-			elsif(state_counter = "1111") --15
-				then
-					a <= '0';
-					b <= '0';
-					c <= '0';
-					d <= '0';
-					sel2_1 <= '1';
-					sel4_1 <= '1';
-					selPar <= '1';
-					comp_rst <='1';
-		END IF;
-	END PROCESS;
+	a <= (not sc(2) and sc(0)) or (not sc(3) and not sc(2) and sc(1)) or (not sc(3) and sc(1) and sc(0)) or (sc(3) and not sc(2) and not sc(1));
+	b <= (not sc(3) and not sc(1) and sc(0)) or (not sc(3) and sc(2) and not sc(1)) or (not sc(3) and sc(2) and sc(0)) or (sc(3) and not sc(2) and not sc(0)) or (sc(3) and not sc(2) and sc(1));
+	c <= (not sc(2) and sc(1) and not sc(0)) or (not sc(3) and sc(2) and not sc(0)) or (not sc(3) and sc(2) and sc(1)) or (sc(3) and not sc(2) and sc(0));
+	d <= (sc(3) and not sc(2)) or (not sc(2) and sc(1) and sc(0)) or (not sc(3) and sc(2) and not sc(1) and sc(0)) or (not sc(3) and sc(2) and sc(1) and not sc(0));
+	sel2_1 <= sc(3) and sc(2);
+	sel4_1 <= sc(3) and sc(2) and sc(1);
+	selPar <= sc(3) and sc(2) and sc(0);
+	comp_rst <= not sc(3) and not sc(2) and not sc(1) and not sc(0);
 
 END behavior;
--- hammingCodeBitSerialEncoder.vhd
+++ hammingCodeBitSerialEncoder.vhd
@@ -33,7 +33,7 @@ ARCHITECTURE bitSerialEncoding of hammingCodeBitSerialEncoder IS
           out1    : out std_logic);
 	END COMPONENT;
 	COMPONENT controlUnit 
-		PORT (state_counter: in std_logic_Vector(3 downto 0);
+		PORT (sc: in std_logic_Vector(3 downto 0);
 				comp_rst, a, b, c, d,sel2_1, sel4_1, selPar:      OUT STD_LOGIC);
 	END COMPONENT;
 	COMPONENT binCounter_4bit 
@@ -44,7 +44,7 @@ ARCHITECTURE bitSerialEncoding of hammingCodeBitSerialEncoder IS
 	
 	signal s_a, s_b, s_c, s_d, a_y, b_y, c_y, d_y, s_reset, s_sel2_1, s_sel4_1, s_selPar: std_logic;
 	signal Q_x12, Q_x13, Q_x14, Q_x15, sa_12, sb_13, sc_14, sd_15, out2_1, out4_1: std_logic;
-	signal state_counter_signal: std_logic_vector(3 downto 0);
+	signal state_counter_signal : std_logic_vector(3 downto 0);
 
 BEGIN
 
@@ -61,10 +61,10 @@ BEGIN
 	xor_c: gateXOR PORT MAP(c_y, Q_x14,sc_14);
 	xor_d: gateXOR PORT MAP(d_y, Q_x15,sd_15);
 	
-	ff_x12: flipFlopDPET PORT MAP(clk,sa_12,'1',s_reset,Q_x12);
-	ff_x13: flipFlopDPET PORT MAP(clk,sb_13,'1',s_reset,Q_x13);
-	ff_x14: flipFlopDPET PORT MAP(clk,sc_14,'1',s_reset,Q_x14);
-	ff_x15: flipFlopDPET PORT MAP(clk,sd_15,'1',s_reset,Q_x15);
+	ff_x12: flipFlopDPET PORT MAP(clk,sa_12,'1',not s_reset,Q_x12);
+	ff_x13: flipFlopDPET PORT MAP(clk,sb_13,'1',not s_reset,Q_x13);
+	ff_x14: flipFlopDPET PORT MAP(clk,sc_14,'1',not s_reset,Q_x14);
+	ff_x15: flipFlopDPET PORT MAP(clk,sd_15,'1',not s_reset,Q_x15);
 	
 	multiplexer2_1: Mux2_1 PORT MAP(s_sel2_1, y, out4_1, out2_1);
 	multiplexer4_1: Mux4_1 PORT MAP(s_sel4_1, s_selPar, Q_x12, Q_x13, Q_x14, Q_x15, out4_1);
